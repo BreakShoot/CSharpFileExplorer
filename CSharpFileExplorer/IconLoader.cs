12d6093b209f29b9513e0f19ce94415e5ee53440
Anthony <12345787+BreakShoot@users.noreply.github.com>
2020-07-21 19:10:25 -0400

    Add project files.

---
+++ CSharpFileExplorer/IconLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace CSharpFileExplorer
+{
+    class IconLoader
+    {
+        public static Icon GetStockIcon(NativeMethods.SHSTOCKICONID siid)
+        {
+            NativeMethods.SHSTOCKICONINFO stockIconInfo = new NativeMethods.SHSTOCKICONINFO();
+            stockIconInfo.cbSize = (uint)Marshal.SizeOf<NativeMethods.SHSTOCKICONINFO>();
+            NativeMethods.SHGetStockIconInfo(siid, (uint)NativeMethods.SHGFI.Icon, ref stockIconInfo);
+            var icon = Icon.FromHandle(stockIconInfo.hIcon).ToBitmap(); //convert to bitmap to make it transparent
+            icon.MakeTransparent();
+            NativeMethods.DestroyIcon(stockIconInfo.hIcon);
+            return Icon.FromHandle(icon.GetHicon()); //back to icon
+        }
+
+        public static Icon GetFileTypeIcon(string filetype)
+        {
+            NativeMethods.SHFILEINFO iconInfo = new NativeMethods.SHFILEINFO();
+            NativeMethods.SHGetFileInfo(filetype, 256, ref iconInfo, 0, (uint)(NativeMethods.SHGFI.Icon | NativeMethods.SHGFI.SmallIcon | NativeMethods.SHGFI.UseFileAttributes));
+            var icon = Icon.FromHandle(iconInfo.hIcon).ToBitmap();
+            icon.MakeTransparent();
+            NativeMethods.DestroyIcon(iconInfo.hIcon);
+            return Icon.FromHandle(icon.GetHicon());
+        }
+    }
+}
